a0bc171038203d52930713f4e08b843cbe6a6a83
керимов алихан <yakhya2809@gmail.com>
2022-06-12 21:07:40 +0300

    Изменил мидлвеар для отлова ошибок

--- models/middlewares/auth.middlewares.js
+++ models/middlewares/auth.middlewares.js
@@ -5,17 +5,23 @@ module.exports = async (req, res, next) => {
     const { authorization } = req.headers;
 
     if (!authorization) {
-      return res.status(401).json("Нет авторизации");
+      return res.status(401).json({
+        error: "Нету авторизации",
+      });
     }
-
     const [type, token] = authorization.split(" ");
     if (type !== "Bearer") {
-      return res.status(401).json("Неверный тип токена");
+      return res.status(401).json({
+        error: "Неверный тип токена ",
+      });
     }
 
     req.user = await jwt.verify(token, process.env.SECRET_JWT_KEY);
+
     next();
   } catch (error) {
-    return res.status(401).json("Ошибка авторизации:" + error.toString());
+    return res.status(401).json({
+      error: "Netu  Tokena" + error.toString(),
+    });
   }
 };
