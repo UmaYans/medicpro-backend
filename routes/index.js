7b1647365d1b0f1863420b49a335d94d4c75334f
керимов алихан <yakhya2809@gmail.com>
2022-06-12 20:53:43 +0300

    что то

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = Router()
 
 router.use(require('./doc.route'))
 router.use(require('./users.route'))
+router.use(require('./comment.routes'))
+router.use(require('./category.routes'))
 
 module.exports = router;
