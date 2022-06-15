cf627226947d0fbb07bdad144a3914d46c5424f3
AlievIB <020alievib@gmail.com>
2022-06-15 16:40:40 +0300

    вывод докторов по клинике

--- routes/doc.route.js
+++ routes/doc.route.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get("/docs", docsController.getDocs);
 router.get("/docs/:id", docsController.getDocsById);
+router.get("/docPlace/:id", docsController.getDocsPlaceId);
 router.post("/docs", docsController.postDoc);
 
 module.exports = router;
