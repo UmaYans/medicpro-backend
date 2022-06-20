0fa5619c24b9001db397ae878ed29c6c3c350ac0
керимов алихан <yakhya2809@gmail.com>
2022-06-20 15:30:02 +0300

    re

--- routes/services.route.js
+++ routes/services.route.js
@@ -6,6 +6,6 @@ const router = Router();
 router.post("/service", ServiceController.postService);
 router.get("/service", ServiceController.getService);
 router.get("/service/:id", ServiceController.getServiceById);
-router.get("service/doc/:id", ServiceController.getServiceDocId);
+router.get("/service/doc/:id", ServiceController.getServiceDocId);
 
 module.exports = router;
